d0a022b4b224227572baf985442d2a9f116e052f
James Horan <James Horan>
2026-02-07 16:45:52 -0600

    fix(app): fixed error with field name change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ const expenses = await getExpenses();
 export default function Home() {
   return (
     <div>
-      {expenses?.map(exp => (<p key={exp.id}>{exp.title}</p>))}
+      <div>
+        {expenses?.map(exp => (
+          <p key={exp.id}>{exp.description}</p>
+        ))}
+      </div>
     </div>
   );
 }
