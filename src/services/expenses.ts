258c6cb6cb048bc81b907a22e589e217fcb41cdb
James Horan <James Horan>
2026-02-07 13:52:00 -0600

    refactor(app): improved naming

--- src/services/expenses.ts
+++ src/services/expenses.ts
@@ -2,7 +2,7 @@ import { prisma } from "../lib/prisma";
 
 export async function getExpenses() {
   try {
-    const expenses = prisma.expense.findMany()
+    const expenses = prisma.expenses.findMany()
     return expenses;
   } catch {
     console.error("Failed to fetch expenses");
