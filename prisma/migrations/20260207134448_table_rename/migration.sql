87efcd41257efe6563bfdafb4be9eac059fdb469
James Horan <James Horan>
2026-02-07 13:48:42 -0600

    refactor(db): created new migration for renamed table

---
+++ prisma/migrations/20260207134448_table_rename/migration.sql
@@ -0,0 +1,13 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "expenses" (
+    "title" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
